f9b19a1d032405badaf182d28ff0294e2de96378
Victor <victor5055@outlook.com>
2022-09-24 21:42:09 -0400

    Adding Js questions and fixing errors in HTML.

---
+++ questions.js
@@ -0,0 +1,13 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
